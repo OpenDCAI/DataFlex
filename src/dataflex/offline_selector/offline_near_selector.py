fe16bc3d1816478c27fc1bceebb48c46b5e31524
wiILIL <975202246@qq.com>
2025-11-17 22:47:39 +0800

    Add offline_ near_selector and Revise offline_tsds_selector (#26)

--- src/dataflex/offline_selector/offline_near_selector.py
+++ src/dataflex/offline_selector/offline_near_selector.py
@@ -31,29 +31,21 @@ class FaissIndexIVFFlat:
         query = np.ascontiguousarray(query.astype(np.float32))
         return self.index.search(query, K)
 
-class offline_tsds_Selector:
+class offline_near_Selector:
     def __init__(self,
                  candidate_path = "/mnt/DataFlow/yry/DataFlex-origin/data/openhermes_10000.json",
                  query_path: str = "/mnt/DataFlow/yry/DataFlex-origin/data/mmlu_pro.json",
                  embed_model: str = "/mnt/DataFlow/yry/model/qwen3-embedding-0.6B",
                  batch_size: int = 32,
-                 save_probs_path: str = "tsds_probs.npy",
-                 max_K: int = 128,
-                 kde_K: int = 64,
-                 sigma: float = 0.8,
-                 alpha: float = 0.5,
-                 C: float = 10.0):
+                 save_indices_path: str = "top_indices.npy",
+                 max_K: int = 1000):
         
         self.candidate_path = candidate_path
         self.query_path = query_path
         self.embed_model = embed_model
         self.batch_size = batch_size
-        self.save_probs_path = save_probs_path
+        self.save_indices_path = save_indices_path
         self.max_K = max_K
-        self.kde_K = kde_K
-        self.sigma = sigma
-        self.alpha = alpha
-        self.C = C
 
     # ---------- 数据加载方法 ----------
     def _load_alpaca_json(self, path):
@@ -95,7 +87,7 @@ class offline_tsds_Selector:
         # --------------------------------------    
         return np.ascontiguousarray(embs)
 
-    # ---------- TSDS 调用接口 ----------
+    # ---------- 调用接口 ----------
     def candidate_sentence_embedding(self):
         texts = self._load_alpaca_json(self.candidate_path)
         logger.info(f"Loaded {len(texts)} candidates")
@@ -110,17 +102,16 @@ class offline_tsds_Selector:
             texts = self._load_alpaca_json(self.candidate_path)[:100]
         return self._embed_texts(texts)
 
-    # ---------- tsds 主程序 ----------
+    # ---------- 主程序 ----------
     def selector(self):
         
-        logger.info("Start loading embeddings for TSDS...")
+        logger.info("Start loading embeddings for nearest_selector...")
         xb = self.candidate_sentence_embedding()  
         xq = self.query_sentence_embedding()      
 
         M, N = xq.shape[0], xb.shape[0]
         MAX_K = self.max_K
-        KDE_K = self.kde_K
-
+        
         logger.info("Building FAISS index...")
         index = FaissIndexIVFFlat(xb)
 
@@ -128,77 +119,29 @@ class offline_tsds_Selector:
         top_dists2, top_indices = index.search(xq, MAX_K)
         sorted_indices = np.argsort(top_dists2, axis=-1)
         static_idx = np.indices(top_dists2.shape)[0]
-        top_dists = np.sqrt(top_dists2[static_idx, sorted_indices])
+        # top_dists = np.sqrt(top_dists2[static_idx, sorted_indices])
         top_indices = top_indices[static_idx, sorted_indices].astype(int)
 
-        if self.sigma == 0:
-            logger.info("Sigma=0, using unity KDE")
-            top_kdes = np.ones_like(top_indices)
-        else:
-            logger.info("Computing KDE...")
-            uniq_ids = list(set(top_indices.reshape(-1)))
-            sub_xb = xb[uniq_ids]
-            index_kde = FaissIndexIVFFlat(sub_xb)
-            D2, _ = index_kde.search(sub_xb, KDE_K)
-            kernel = np.maximum(0.0, 1 - D2 / (self.sigma ** 2))
-            kde = kernel.sum(axis=1)
-            kde_map = {uniq_ids[i]: kde[i] for i in range(len(uniq_ids))}
-            top_kdes = np.vectorize(lambda t: kde_map[t])(top_indices)
+        #保存每个query最近的前MAX_K个索引
+        assert isinstance(top_indices, np.ndarray), \
+            "top_indices must be a numpy array"
+        assert top_indices.shape == (M, MAX_K), \
+            f"Expected shape {(M, MAX_K)}, got {top_indices.shape}"
+        assert np.issubdtype(top_indices.dtype, np.integer), \
+            f"Expected integer dtype, got {top_indices.dtype}"
 
-        logger.info("Running heap optimization for TSDS...")
-        lastK = [0] * M
-        heap = [(1.0 / top_kdes[j][0], 0, j) for j in range(M)]
-        heapq.heapify(heap)
-        cost = np.zeros(M)
-        dist_wsum = [top_dists[j][0] / top_kdes[j][0] for j in range(M)]
-        total_cost, s = 0.0, 0.0
-
-        while heap:
-            count, k, j = heapq.heappop(heap)
-            s = count
-            total_cost -= cost[j]
-            cost[j] = top_dists[j][k + 1] * count - dist_wsum[j]
-            total_cost += cost[j]
-            if self.alpha / self.C * total_cost >= (1 - self.alpha) * M:
-                break
-            lastK[j] = k
-            if k < MAX_K - 2:
-                count += 1.0 / top_kdes[j][k + 1]
-                heapq.heappush(heap, (count, k + 1, j))
-                dist_wsum[j] += top_dists[j][k + 1] / top_kdes[j][k + 1]
-
-        logger.info("Calculating global sampling probability...")
-        global_probs = np.zeros(N, dtype=np.float64)
-        inv_M = 1.0 / M
-        for j in range(M):
-            psum = 0.0
-            for k in range(lastK[j] + 1):
-                w = inv_M / s / top_kdes[j][k]
-                global_probs[top_indices[j][k]] += w
-                psum += w
-            global_probs[top_indices[j][lastK[j] + 1]] += max(inv_M - psum, 0)
-
-        global_probs = np.maximum(global_probs, 0)
-        global_probs /= global_probs.sum()
-
-        # 保存为 npy 文件
-        np.save(self.save_probs_path, global_probs)
-        logger.info(f"TSDS probs saved to {self.save_probs_path}")
-
-        return global_probs
+        np.save(self.save_indices_path, top_indices)
+        logger.info(f"Saved to  {self.save_indices_path}")
 
 
 if __name__ == "__main__":
-    tsds = offline_tsds_Selector(
+    near = offline_near_Selector(
         candidate_path="/mnt/DataFlow/yry/DataFlex-yry/data/openhermes_100000.json",
         query_path="/mnt/DataFlow/yry/data_new/MMLU__merged.json",
         embed_model="vllm:/mnt/DataFlow/yry/model/qwen3-embedding-0.6B",
         batch_size=32,
-        save_probs_path="tsds_probs.npy",
-        max_K=5000,
-        kde_K=1000,
-        sigma=0.75,
-        alpha=0.6,
-        C=5.0
+        save_indices_path="top_indices.npy",
+        max_K=1000,
+        
     )
-    tsds.selector()
+    near.selector()
